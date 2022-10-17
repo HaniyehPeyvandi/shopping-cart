657fecb468d3e6d815ba2865b870e389894f380a
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-17 23:25:23 +0330

    add CartPage component

---
+++ front-end/src/pages/CartPage/CartPage.js
@@ -0,0 +1,11 @@
+import Layout from "../../Layout/Layout";
+
+const CartPage = () => {
+  return (
+    <Layout>
+      <div>This is cart page</div>
+    </Layout>
+  );
+};
+
+export default CartPage;
