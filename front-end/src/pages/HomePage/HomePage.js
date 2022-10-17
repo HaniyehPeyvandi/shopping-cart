fc6db1617381a60d099d248f62722ef26c2b3966
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-17 23:24:59 +0330

    add HomePage component

---
+++ front-end/src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import Layout from "../../Layout/Layout";
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <div>This is home page</div>
+    </Layout>
+  );
+};
+
+export default HomePage;
