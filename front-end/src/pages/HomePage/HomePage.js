3827d22a4e5057cae89975c0666ce4a73c67b418
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-18 12:41:24 +0330

    add products to HomePage

--- front-end/src/pages/HomePage/HomePage.js
+++ front-end/src/pages/HomePage/HomePage.js
@@ -1,9 +1,30 @@
 import Layout from "../../Layout/Layout";
+import styles from "./HomePage.module.css";
+import * as data from "../../data";
 
 const HomePage = () => {
+  const addProductHandler = (product) => {
+    console.log(product);
+  }
+
   return (
     <Layout>
-      <div>This is home page</div>
+      <main className={styles.container}>
+        <section className={styles.productList}>
+          {data.products.map((p) => (
+            <section key={p.id} className={styles.product}>
+              <div>
+                <img src={p.image} alt={p.name} />
+              </div>
+              <div className={styles.productDesc}>
+                <p>{p.name}</p>
+                <p>$ {p.price}</p>
+                <button className={styles.btn} onClick={() => addProductHandler(p)}>Add to Cart</button>
+              </div>
+            </section>
+          ))}
+        </section>
+      </main>
     </Layout>
   );
 };
