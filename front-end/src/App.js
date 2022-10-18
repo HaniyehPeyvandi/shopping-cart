aaa14a7e7d5ca3393a7c95b6111a19e9dfebb229
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-18 13:33:51 +0330

    add CartContext to the project

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import CartPage from "./pages/CartPage/CartPage";
 import HomePage from "./pages/HomePage/HomePage";
+import CartProvider from "./Providers/CartProvider";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/cart" element={<CartPage/>} />
-      <Route path="/" element={<HomePage/>} />
-    </Routes>
+    <CartProvider>
+      <Routes>
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </CartProvider>
   );
 }
 
