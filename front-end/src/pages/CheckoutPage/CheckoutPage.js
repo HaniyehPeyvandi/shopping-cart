184eb1f5a02c1305dd253b0aa5cb01f16f2c395e
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-19 21:48:14 +0330

    add CheckoutPage

---
+++ front-end/src/pages/CheckoutPage/CheckoutPage.js
@@ -0,0 +1,11 @@
+import Layout from "../../Layout/Layout";
+
+const CheckoutPage = () => {
+  return ( 
+    <Layout>
+      <div>This is checkout page</div>
+    </Layout>
+   );
+}
+ 
+export default CheckoutPage;
