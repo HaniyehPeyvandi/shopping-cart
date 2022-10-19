ba7b436d562080722d6d413e0258b97cfdf4cfd7
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-19 12:44:59 +0330

    add code for showing cart items quantity in navigation

--- front-end/src/components/Navigation/Navigation.js
+++ front-end/src/components/Navigation/Navigation.js
@@ -1,19 +1,33 @@
 import { NavLink } from "react-router-dom";
+import { useCart } from "../../Providers/CartProvider";
 import styles from "./Navigation.module.css";
 
 const Navigation = () => {
+  const { cart } = useCart();
+
   return (
     <header className={styles.mainNavigation}>
       <nav>
         <ul>
           <li>
-            <NavLink to="/" end className={({isActive}) => isActive ? styles.activeLink : ""}>Home</NavLink>
+            <NavLink
+              to="/"
+              end
+              className={({ isActive }) => (isActive ? styles.activeLink : "")}
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/cart" className={({isActive}) => isActive ? styles.activeLink : ""}>Cart</NavLink>
+            <NavLink
+              to="/cart"
+              className={({ isActive }) => (isActive ? styles.activeLink : "")}
+            >
+              Cart <span className={styles.cartBadge}>{cart.length}</span>
+            </NavLink>
           </li>
         </ul>
-        <div>Hani Shop</div>
+        {/* <div>Hani Shop</div> */}
       </nav>
     </header>
   );
