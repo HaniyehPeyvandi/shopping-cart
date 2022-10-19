8d8d0b5afd7df595efce1adb20ab67b7c2b42689
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-19 13:29:44 +0330

    add code for showing notifications

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,10 +3,13 @@ import "./App.css";
 import CartPage from "./pages/CartPage/CartPage";
 import HomePage from "./pages/HomePage/HomePage";
 import CartProvider from "./Providers/CartProvider";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <CartProvider>
+      <ToastContainer />
       <Routes>
         <Route path="/cart" element={<CartPage />} />
         <Route path="/" element={<HomePage />} />
--- front-end/src/pages/HomePage/HomePage.js
+++ front-end/src/pages/HomePage/HomePage.js
@@ -2,19 +2,23 @@ import Layout from "../../Layout/Layout";
 import styles from "./HomePage.module.css";
 import * as data from "../../data";
 import { useCart, useCartActions } from "../../Providers/CartProvider";
-
+import { toast } from "react-toastify";
 
 const HomePage = () => {
   const dispatch = useCartActions();
-  const {cart} = useCart();
+  const { cart } = useCart();
 
   const addProductHandler = (product) => {
     dispatch({ type: "ADD_TO_CART", payload: product });
+    toast.success(`${product.name} added to cart`, {
+      autoClose: 3000,
+      theme: "colored",
+    });
   };
 
-  const checkInCart = (cart,product) => {
-    return cart.find(item => item.id === product.id);
-  }
+  const checkInCart = (cart, product) => {
+    return cart.find((item) => item.id === product.id);
+  };
 
   return (
     <Layout>
@@ -32,7 +36,7 @@ const HomePage = () => {
                   className={styles.btn}
                   onClick={() => addProductHandler(p)}
                 >
-                  {checkInCart(cart,p) ? "In Cart" : "Add to Cart"}
+                  {checkInCart(cart, p) ? "In Cart" : "Add to Cart"}
                 </button>
               </div>
             </section>
