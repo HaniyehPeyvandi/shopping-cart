f5985320d7a3c3a5286ac2acb5291c5898a6714f
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-19 22:08:09 +0330

    add SignupPage and its route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,12 +7,14 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
 
 function App() {
   return (
     <CartProvider>
       <ToastContainer />
       <Routes>
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/cart" element={<CartPage />} />
---
+++ front-end/src/pages/SignupPage/SignupPage.js
@@ -0,0 +1,7 @@
+import Layout from "../../Layout/Layout";
+
+const SignupPage = () => {
+  return <Layout>Signup Page</Layout>;
+};
+
+export default SignupPage;
