42ee3252c7481c66cd5a75171712458f58d528dd
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-20 12:40:55 +0330

    add SignupForm component and form validation

---
+++ front-end/src/components/SignupForm/SignupForm.js
@@ -0,0 +1,79 @@
+import Input from "../../common/Input/Input";
+import styles from "./SignupForm.module.css";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+  name: "",
+  email: "",
+  phoneNumber: "",
+  password: "",
+  passwordConfirm: "",
+};
+
+const onSubmit = (values) => {
+  console.log(values);
+};
+
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .required("Name is required")
+    .min(3, "Name length is not valid"),
+  email: Yup.string()
+    .email("Email format is invalid")
+    .required("Email is required"),
+  phoneNumber: Yup.string()
+    .required("Phone Number is required")
+    .matches(/^[0-9]{11}$/, "Phone Number is invalid")
+    .nullable(),
+  password: Yup.string().required("Password is required"),
+  // .matches(
+  //   /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+  //   "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+  // )
+  passwordConfirm: Yup.string()
+    .required("Pasword Confirmation is required")
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
+});
+
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
+    validateOnMount: true,
+    enableReinitialize: true,
+  });
+
+  return (
+    <div className={styles.formContainer}>
+      <form onSubmit={formik.handleSubmit}>
+        <Input formik={formik} name="name" label="Name" />
+        <Input formik={formik} name="email" label="Email" type="email" />
+        <Input
+          formik={formik}
+          name="phoneNumber"
+          label="Phone Number"
+          type="tel"
+        />
+        <Input
+          formik={formik}
+          name="password"
+          label="Password"
+          type="password"
+        />
+        <Input
+          formik={formik}
+          name="passwordConfirm"
+          label="Password confirmation"
+          type="password"
+        />
+        <button type="submit" disabled={!formik.isValid} className={styles.btn}>
+          Signup
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignupForm;
