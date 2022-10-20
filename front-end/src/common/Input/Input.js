17fe78deecaaa2de7f7161859dc3e4d1009a2ee3
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-20 12:38:11 +0330

    add Input component

---
+++ front-end/src/common/Input/Input.js
@@ -0,0 +1,20 @@
+import styles from "./Input.module.css";
+
+const Input = ({ label, name, formik, type = "text" }) => {
+  return (
+    <div className={styles.formControl}>
+      <label htmlFor={name}>{label}</label>
+      <input
+        id={name}
+        type={type}
+        {...formik.getFieldProps(name)}
+        name={name}
+      />
+      {formik.errors[name] && formik.touched[name] && (
+        <div className={styles.error}>{formik.errors[name]}</div>
+      )}
+    </div>
+  );
+};
+
+export default Input;
