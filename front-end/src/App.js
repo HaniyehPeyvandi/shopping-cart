4529fc037673462813c42e569b3afc5802ade41d
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-11-13 22:16:56 +0330

    add AuthContext to the project

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,19 +8,22 @@ import "react-toastify/dist/ReactToastify.css";
 import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignupPage from "./pages/SignupPage/SignupPage";
+import AuthProvider from "./Providers/AuthProvider";
 
 function App() {
   return (
-    <CartProvider>
-      <ToastContainer />
-      <Routes>
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <ToastContainer />
+        <Routes>
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
