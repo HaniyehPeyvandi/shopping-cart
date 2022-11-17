a457fcf2ff15f6b27b5a9377025c138b1fb7ecc7
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-11-17 23:30:33 +0330

    get all products from backend

--- front-end/src/pages/HomePage/HomePage.js
+++ front-end/src/pages/HomePage/HomePage.js
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import Layout from "../../Layout/Layout";
 import styles from "./HomePage.module.css";
-import * as data from "../../data";
 import { useCart, useCartActions } from "../../Providers/CartProvider";
 import { toast } from "react-toastify";
+import { getAllProducts } from "../../services/getAllProductsService";
 
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   const dispatch = useCartActions();
   const { cart } = useCart();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data } = await getAllProducts();
+        setLoading(false);
+        setProducts(data);
+        setError(null);
+      } catch (error) {
+        setLoading(false);
+        setError(error.message);
+        setProducts([]);
+      }
+    };
+
+    setLoading(true);
+    fetchProducts();
+  }, []);
 
   const addProductHandler = (product) => {
     dispatch({ type: "ADD_TO_CART", payload: product });
@@ -24,32 +47,35 @@ const HomePage = () => {
     <Layout>
       <main className={styles.container}>
         <section className={styles.productList}>
-          {data.products.map((p) => (
-            <section key={p.id} className={styles.product}>
-              <div className={styles.productImg}>
-                <img src={p.image} alt={p.name} />
-              </div>
-              <div className={styles.productDesc}>
-                <p>{p.name}</p>
-                <p className={styles.productPrice}>
-                  {p.discount > 0 ? (
-                    <>
-                      <del>$ {p.price}</del>
-                      <span>$ {p.offPrice}</span>
-                    </>
-                  ) : (
-                    <span>$ {p.price}</span>
-                  )}
-                </p>
-                <button
-                  className={styles.btn}
-                  onClick={() => addProductHandler(p)}
-                >
-                  {checkInCart(cart, p) ? "In Cart" : "Add to Cart"}
-                </button>
-              </div>
-            </section>
-          ))}
+          {loading && <p style={{ textAlign: "center" }}>Loading...</p>}
+          {error && <p style={{ textAlign: "center" }}>{error}</p>}
+          {products.length > 0 &&
+            products.map((p) => (
+              <section key={p._id} className={styles.product}>
+                <div className={styles.productImg}>
+                  <img src={p.image} alt={p.name} />
+                </div>
+                <div className={styles.productDesc}>
+                  <p>{p.name}</p>
+                  <p className={styles.productPrice}>
+                    {p.discount > 0 ? (
+                      <>
+                        <del>$ {p.price}</del>
+                        <span>$ {p.offPrice}</span>
+                      </>
+                    ) : (
+                      <span>$ {p.price}</span>
+                    )}
+                  </p>
+                  <button
+                    className={styles.btn}
+                    onClick={() => addProductHandler(p)}
+                  >
+                    {checkInCart(cart, p) ? "In Cart" : "Add to Cart"}
+                  </button>
+                </div>
+              </section>
+            ))}
         </section>
       </main>
     </Layout>
